8a4141dd7cde2e2a1b59824968c0fa8d4287756a
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2021-12-17 14:52:29 -0500

    porfin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var corsOptions = {
 }
 //Creacion de App y Ruta
 const app = express()
-const port = process.env.port || 4000;
+const port = process.env.port || 8000;
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
